3e8a8b890bbc3bc66210423b23839964001d2a95
Elitembed <shayne@shaynetaylor.com>
2024-01-28 17:29:04 -0600

    Branding Removal

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -543,7 +543,6 @@ export const Bot = (botProps: BotProps & { class?: string }) => {
             </div>
           </Show>
         </Show>
-        test
         <BottomSpacer ref={bottomSpacer} />
       </div>
       {sourcePopupOpen() && <Popup isOpen={sourcePopupOpen()} value={sourcePopupSrc()} onClose={() => setSourcePopupOpen(false)} />}
